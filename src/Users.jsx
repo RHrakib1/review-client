109e6d4f692389908bcaeaa527b57bbdc3976512
Md Rakib Hasan <rakibhasanmd457@gmail.com>
2025-09-12 15:32:48 +0600

    add to the delete

--- src/Users.jsx
+++ src/Users.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useLoaderData } from 'react-router-dom'
+import Swal from 'sweetalert2'
 
 export default function Users() {
     const loaderuse = useLoaderData()
+    const [deleteCrl, setdeleteCrl] = useState(loaderuse)
+    const heandledelete = id => {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/userserver/${id}`, {
+                    method: "DELETE"
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        if (data.deletedCount > 0) {
+                            Swal.fire("Deleted!", "Your file has been deleted.", "success")
+                            const remaining = deleteCrl.filter(d => d._id !== id)
+                            setdeleteCrl(remaining)
+                        }
+                    })
+            }
+        })
+    }
     return (
         <div>
             <h1>Data {loaderuse.length}</h1>
@@ -19,10 +46,10 @@ export default function Users() {
                     <tbody>
                         {/* row 1 */}
                         {
-                            loaderuse.map(data => <tr>
+                            deleteCrl.map(data => <tr>
                                 <td>{data.email}</td>
                                 <td>{data.createtime}</td>
-                                <td>Blue</td>
+                                <td className='cursor-pointer' onClick={() => heandledelete(data._id)}>X</td>
                             </tr>)
                         }
                     </tbody>
